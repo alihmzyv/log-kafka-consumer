d3de0cdabe0c04ccd34cfb00e93feefa4e9caf01
alihmzyv <alihmzyv@gmail.com>
2023-05-03 17:34:24 +0400

    config

---
+++ src/main/java/com/alihmzyv/logkafkaconsumer/LogKafkaConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.alihmzyv.logkafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class LogKafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogKafkaConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alihmzyv/logkafkaconsumer/LogKafkaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alihmzyv.logkafkaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogKafkaConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
